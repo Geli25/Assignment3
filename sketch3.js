b7f2a38f31f3c60cd2d26f031bc15c4d13dadebd
Angelina <h1255513@gmail.com>
2018-03-21 11:58:09 -0400

    updated sketch

--- sketch3.js
+++ sketch3.js
@@ -1,4 +1,3 @@
-
 var table;
 var bubbles=[];
 
@@ -13,38 +12,85 @@ function setup(){
 }
 
 function draw(){
-    background(0);
+    background('#34373b');
 
     for (var i = 0; i<bubbles.length; i++){
+        bubbles[i].fall();
         bubbles[i].display();
     }
 }
 
 function loadData(){
-    for (var i = 0;i < table.getRowCount(); i++){
-        var row = table.getRow(i);
-        var ATIC = row.get('American Tropical Interpretive Center');
-        var AA = row.get('Avila Adobe');
-        var CA = row.get('Chinese American Museum');
-    }
 
-    bubbles[i]=new Bubble(random(0,800), random(0,800), ATIC/100, AA/100, CA/100);
+    for (var a = 0;a < table.getRowCount(); a++){
+        var row = table.getRow(a);
+        var AA = row.get('Avila Adobe');
+        var month = row.get('Month');
+        bubbles[a]=new Bubble(random(0,200), random(0,200), random(2,10), AA/200, month);
+    }
 }
 
 class Bubble{
-    constructor(tempX,tempY,tempR,tempATIC,tempAA,tempCA){
-        this.x = tempX;
-        this.y = tempY;
-        this.r = tempR;
-        this.ATIC = Number(tempATIC);
+    constructor(tempX,tempY,tempR,tempAA,tempMonth,speed){
+        this.x = random(0,700);
+        this.y = random(-500,200);
+        this.r = random(2,10);
         this.AA = Number(tempAA);
-        this.CA = Number(tempCA);
+        this.month=Number(tempMonth);
+    }
+
+    fall(){
+        this.y+=this.month;
+        if (this.y>700){
+            this.y= -this.y;
+        }
+        if(mouseIsPressed){
+            this.y= this.y;
+        }
     }
 
     display(){
-        ellipse(this.x, this.y, this.AA, this.AA);
-        rect (this.x,this.y,this.ATIC, this.ATIC);
-        ellipse(this.x,this.y,this.CA,this.CA);
+        stroke(255,255,255);
+        strokeWeight(3);
+        if (this.month==1){
+            fill('#2ecc71');
+        }
+        if (this.month==2){
+            fill('#3498db');
+        }
+        if (this.month==3){
+            fill('#9b59b6');
+        }
+        if (this.month==4){
+            fill('#6c7a89');
+        }
+        if (this.month==5){
+            fill('#f2ca27');
+        }
+        if (this.month==6){
+            fill('#e67e22');
+        }
+        if (this.month==7){
+            fill ('#e74c3c');
+        }
+        if (this.month==8){
+            fill('#cc3f5c');
+        }
+        if (this.month==9){
+            fill('#99a6af');
+        }
+        if (this.month==10){
+            fill('#93B3AF');
+        }
+        if (this.month==11){
+            fill('#DC4E1B');
+        }
+        if (this.month==12){
+            fill(255,0,13);
+        }
+        rotate(radians(this.month));
+        rect(this.x, this.y, this.month*10, this.AA);
+
     }
 
 }
